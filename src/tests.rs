b151a22dae87182085545ef199d783b93489feee
fabian schuller <fabian.schuller@adesso.com>
2024-02-20 14:36:05 +0100

    no more video tests for github actions

--- src/tests.rs
+++ src/tests.rs
@@ -1,5 +1,11 @@
+use std::sync::mpsc;
 use std::sync::Arc;
 use std::sync::RwLock;
+use std::thread;
+use std::time::Instant;
+use std::ops::Deref;
+
+use sdl2::pixels::Color;
 
 use crate::engine_pa::PathtracingObject as PO;
 use crate::engine_pa::PathtracingCamera as PTC;
@@ -20,34 +26,11 @@ pub fn make_polytree_test() {
     let mut p1 : PT = *PT::new(p1);
 }
 
-pub fn init_canvas_test() -> Result<(), String> {
-    let w : usize = 1000;
-    let h : usize = 1000;
-    let sdl_context = sdl2::init()?;
-    let video_subsystem = sdl_context.video()?;
-    let window = video_subsystem.window("rust3d", w as u32, h as u32)
-        .position_centered()
-        .build()
-        .expect("could not initialize video subsystem");
-    let mut canvas = window.into_canvas().build()
-        .expect("could not make a canvas");
-    let mut event_pump = sdl_context.event_pump()?;
-    Ok(())
-}
-
 #[test]
-pub fn render_test() -> Result<(), String> {
+pub fn render_test() {
     let w : usize = 1000;
     let h : usize = 1000;
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
-    let window = video_subsystem.window("rust3d", w as u32, h as u32)
-        .position_centered()
-        .build()
-        .expect("could not initialize video subsystem");
-    let mut canvas = window.into_canvas().build()
-        .expect("could not make a canvas");
-    let mut event_pump = sdl_context.event_pump().unwrap();
+   
     let mut p1 = P::parse_wavefront(&String::from("samples/horse.obj"), &String::from("samples/horse_tex.png"));
     p1.rot(V3{x: 3.14*1.5, y: 0.0, z: 3.14*1.6});
     let mut p1 : PT = *PT::new(p1); 
@@ -55,6 +38,55 @@ pub fn render_test() -> Result<(), String> {
     objs.add(p1);
 	let mut camera : PTC = PTC::new(V3{x: -5.0, y: 0.0, z: 0.0}, 0.0, 0.0, 270.0);
     let objs_arc = Arc::new(RwLock::new(objs));
-    render(&mut canvas, Arc::clone(&objs_arc), camera, &w, &h);
-    Ok(())
+    
+
+    let objs_ = objs_arc.read().unwrap();
+    let section = camera.render_modulus(objs_.deref(), w, h, 3, 10);
+}
+
+#[test]
+pub fn render_multithreading_test() {
+    let w : usize = 1000;
+    let h : usize = 1000;
+   
+    let mut p1 = P::parse_wavefront(&String::from("samples/horse.obj"), &String::from("samples/horse_tex.png"));
+    p1.rot(V3{x: 3.14*1.5, y: 0.0, z: 3.14*1.6});
+    let mut p1 : PT = *PT::new(p1); 
+    let mut objs : POs = POs::new();
+    objs.add(p1);
+    let mut camera : PTC = PTC::new(V3{x: -5.0, y: 0.0, z: 0.0}, 0.0, 0.0, 270.0);
+
+    println!("Setting up threads...");
+    let now = Instant::now();
+
+
+    let objs_arc = Arc::new(RwLock::new(objs));
+    let (tx, rx) = mpsc::channel::<(usize, Vec<Color>)>();
+    let n = 16;
+    let camera_arc = Arc::new(camera);
+
+    for i in 0..n {
+        let objs_arc = Arc::clone(&objs_arc);
+        let camera_arc = Arc::clone(&camera_arc);
+        let tx = tx.clone();
+        let w_ = w.clone();
+        let h_ = h.clone();
+
+        thread::spawn(move || {
+            let objs_ = objs_arc.read().unwrap();
+            let section = camera_arc.render_modulus(objs_.deref(), w_, h_, i, n);
+            tx.send((i.to_owned(), section));
+        });
+    }
+    println!("Setup took {}ms", now.elapsed().as_millis());
+    println!("Started rendering without issues");
+    let now = Instant::now();
+
+    for i in 0..n {
+        let section = rx.recv().unwrap();
+        println!("Thread {} finished rendering", section.0);
+       
+    }
+
+    println!("Render took {}ms", now.elapsed().as_millis());
 }
